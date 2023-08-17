e98d24b42dfffffdc65977c199d1620824ac3353
santhoshkumar <130048133+santhoshkumar2703@users.noreply.github.com>
2023-08-17 17:21:24 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,103 @@
+export const robots = [
+    {
+        id: 1,
+        name: "santhosh kumar",
+        username: "santhosh",
+        email: "santhoshkumar@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Mohammed farhaan",
+        username: "farhaan",
+        email: "mohammedfarhaan@gmail.com"
+    },
+
+    {
+        id: 3,
+        name: "Preethe",
+        username: "preethe",
+        email: "preethe@gmail.com"
+    },
+
+    {
+        id: 4,
+        name: "praveen kumar",
+        username: "praveen",
+        email: "praveenkumar@gmail.com"
+    },
+
+    {
+        id: 5,
+        name: "Anju kurian",
+        username: "Anju",
+        email: "Anjutk@gmail.com"
+    },
+
+
+    {
+        id: 6,
+        name: "Sivamani",
+        username: "Siva",
+        email: "siva@gmail.com"
+    },
+
+
+    {
+        id: 7,
+        name: "Rakesh",
+        username: "Rakhi",
+        email: "Rakesh@gmail.com"
+    },
+
+
+    {
+        id: 9,
+        name: "Srirujith",
+        username: "rujith",
+        email: "ruhjith@gmail.com"
+    },
+
+
+    {
+        id: 10,
+        name: "Vaishali",
+        username: "Vaishu",
+        email: "vaishali@gmail.com"
+    },
+
+    {
+        id: 11,
+        name: "Sharan Madhav",
+        username: "Madhava",
+        email: "sharan@gmail.com"
+    },
+
+    {
+        id: 12,
+        name: "Sathiya",
+        username: "sathiya",
+        email: "sathiya@gmail.com"
+    },
+
+    {
+        id: 13,
+        name: "latha",
+        username: "latha",
+        email: "latha@gmail.com"
+    },
+
+    {
+        id: 14,
+        name: "suvetha",
+        username: "suvetha",
+        email: "suvetha@gmail.com"
+    },
+
+    {
+        id: 15,
+        name: "kartika",
+        username: "kartika",
+        email: "kartika@gmail.com"
+    }
+
+]
